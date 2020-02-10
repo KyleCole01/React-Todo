0fa184f50f9895eaec9027b829fa64f2377ca0c9
KyleCole01 <kyle@misfitselectronics.com>
2020-02-10 13:22:42 -0600

    reach project MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,92 @@
-import React from 'react';
+import React from "react";
+
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import "./components/TodoComponents/Todo.css";
+
+const todos = [
+  {
+    name: "Clean Room",
+    id: 1,
+    completed: false
+  },
+  {
+    name: "Clean Garage",
+    id: 2,
+    completed: false
+  },
+  {
+    name: "Just Clean",
+    id: 3,
+    completed: false
+  },
+  {
+    name: "Just Clean",
+    id: 4,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todoList: todos
+    };
+  }
+
+  toggleItem = clickedId => {
+    const newTodoList = this.state.todoList.map(item => {
+      if (item.id === clickedId) {
+        console.log(item);
+        return {
+          ...item,
+          completed: !item.completed
+        };
+      } else {
+        return item;
+      }
+    });
+
+    this.setState({
+      todoList: newTodoList
+    });
+  };
+  addNewItem = itemText => {
+    const newItem = {
+      id: Date.now(),
+      completed: false,
+      name: itemText
+    };
+    this.setState({
+      todoList: [...this.state.todoList, newItem]
+    });
+  };
+  clearCompleted = () => {
+    const newTodoList = this.state.todoList.filter(item => {
+      if (!item.completed) {
+        return item;
+      } 
+    });
+      console.log(newTodoList, "new list")
+    this.setState({
+      todoList: newTodoList
+    });
+  };
+
   render() {
+    console.log("rendering...");
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1>Todo List</h1>
+          <TodoForm addNewItem={this.addNewItem} />
+        </div>
+        <TodoList
+          todos={this.state.todoList}
+          toggleItem={this.toggleItem}
+          clearCompleted={this.clearCompleted}
+        />
       </div>
     );
   }
